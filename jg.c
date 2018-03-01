367a6746263f3cd98e404afbd6006acf5bcc7166
venkatroshan <35727735+venkatroshan@users.noreply.github.com>
2018-03-01 16:17:43 +0530

    Create jg.c

---
+++ jg.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i,j,temps,n;
+printf("enter number");
+scanf("%d",&n);
+printf("enter  number");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+for(i=0;i<n;i++)
+{
+printf("%d",a[i]);
+}
+}
